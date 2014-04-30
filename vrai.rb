0a598b2db61f32e21e7495d7dd1815c49e46a921
oeihiko <oeihiko@gmail.com>
2014-04-30 17:01:40 -0400

    Did some stuff

--- vrai.rb
+++ vrai.rb
@@ -1,11 +1,29 @@
 
+module EnglishFluent
+  def _split(english)
+    english.delete!("\n")
+    sentences = english.scan(/[^\.?\??!?;?]+[\.?\??!?;?]*/)
+    sentences.map! { |sentence| sentence.scan(/[^\,]+[\,]*/) }
+    sentences.each { |sentence| sentence.map! {|clause| clause.split() }}
+    return sentences
+  end
 
+  def _compile(clause)
+    
+  end
 
+end
 
+class Apple
+  def eat
+    puts "YUM!"
+  end
+end
 
 class Vrai
-
-  def _interpret text
+  include EnglishFluent
+  def _interpret(text)
+    words = _split(text)
   end  
   
 end
@@ -14,11 +32,14 @@ class Self
 
 end
 
-class EnglishFluent
-  def _split(english)
-    english.delete!("\n")
-    sentences = english.scan(/[^\.?\??!?;?]+[\.?\??!?;?]*/)
-    sentences.map! { |sentence| sentence.scan(/[^\,]+[\,]*/) }
-  end
+
+
+v = Vrai.new
+ARGV.each do |arg|
+  p v._interpret arg.dup
 end
+
+
+
+
 
